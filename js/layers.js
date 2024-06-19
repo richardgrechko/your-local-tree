44f0580e99529bcd1cf8774e03f6f751eaa4a5e5
richardgrechko <162537734+richardgrechko@users.noreply.github.com>
2024-06-20 02:14:07 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -10,7 +10,7 @@ addLayer("r", {
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "rebirth points", // Name of prestige currency
     baseResource: "seconds", // Name of resource prestige is based on
-    baseAmount() {return player.r.points}, // Get the current amount of baseResource
+    baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 1, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
@@ -58,7 +58,7 @@ addLayer("p", {
     requires: new Decimal(1000), // Can be a function that takes requirement increases into account
     resource: "prestige points", // Name of prestige currency
     baseResource: "RP", // Name of resource prestige is based on
-    baseAmount() {return player.p.points.add(1)}, // Get the current amount of baseResource
+    baseAmount() {return player.r.points.add(1)}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.75, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
