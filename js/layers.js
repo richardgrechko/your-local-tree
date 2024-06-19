4d9a992b50e1037c28240f95d94f9a93cd0c201e
richardgrechko <162537734+richardgrechko@users.noreply.github.com>
2024-06-20 02:58:12 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -4,7 +4,7 @@ addLayer("r", {
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
-		points: new Decimal(1),
+		points: new Decimal(0),
     }},
     color: "#8000FF",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
@@ -52,7 +52,7 @@ addLayer("p", {
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
-		points: new Decimal(1),
+		points: new Decimal(0),
     }},
     color: "#0080FF",
     requires: new Decimal(1000), // Can be a function that takes requirement increases into account
