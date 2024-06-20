1dba2849eb8d680b10c2bf8b4fef74df2f379e01
richardgrechko <162537734+richardgrechko@users.noreply.github.com>
2024-06-20 21:00:14 +0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -44,6 +44,11 @@ function getPointGen() {
 
 	let gain = new Decimal(1)
 	gain = gain.mul(player.r.points.add(1))
+	gain = gain.mul(buyableEffect('b', 11))
+	gain = gain.mul(buyableEffect('b', 12))
+	gain = gain.mul(buyableEffect('b', 13))
+	gain = gain.mul(buyableEffect('b', 14))
+	gain = gain.mul(buyableEffect('b', 15))
 	return gain
 }
 
