1d0cf547aef2bd1708c590e911ecc03d5114c62f
richardgrechko <162537734+richardgrechko@users.noreply.github.com>
2024-06-20 20:30:06 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -73,7 +73,7 @@ addLayer("r", {
             cost() {return new Decimal(10).mul(getBuyableAmount(this.layer, this.id).add(1))},
             display() {return "("+formatWhole(getBuyableAmount(this.layer, this.id).add(tmp['r'].buyables[11].bonus))+"/"+formatWhole(D(100).add(tmp['r'].buyables[11].bonus))+")<br><h3>Quickener</h3><br>+10% points<br>Cost: "+format(this.cost())+" bytes<br>Currently: "+format(buyableEffect(this.layer, this.id))+"x"},
             effect() {return new Decimal(1).add(new Decimal(0.1).mul(getBuyableAmount(this.layer, this.id).add(tmp['r'].buyables[11].bonus))).mul(buyableEffect('r', 12))},
-            canAfford() {return player.b.points.gte(this.cost())},
+            canAfford() {return player.r.points.gte(this.cost())},
             buy() {
                 if(!hasMilestone('unl', 1)){player[this.layer].points = player[this.layer].points.sub(this.cost())
                 setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(1))} else {
@@ -99,7 +99,7 @@ addLayer("r", {
             cost() {return new Decimal(100).mul(new D(1.12).pow(x || getBuyableAmount(this.layer, this.id)))},
             display() {return "("+formatWhole(getBuyableAmount(this.layer, this.id).add(tmp['r'].buyables[11].bonus))+"/"+formatWhole(D(100).add(tmp['r'].buyables[11].bonus))+")<br><h3>Fastener</h3><br>+10% points<br>Cost: "+format(this.cost())+" bytes<br>Currently: "+format(buyableEffect(this.layer, this.id))+"x"},
             effect() {return new Decimal(5).add(new Decimal(0.5).mul(getBuyableAmount(this.layer, this.id).add(tmp['r'].buyables[11].bonus))).mul(buyableEffect('r', 12))},
-            canAfford() {return player.b.points.gte(this.cost())},
+            canAfford() {return player.r.points.gte(this.cost())},
             buy() {
                 if(!hasMilestone('unl', 1)){player[this.layer].points = player[this.layer].points.sub(this.cost())
                 setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(1))} else {
